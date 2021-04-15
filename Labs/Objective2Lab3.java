68c10129d09c262c2abd440339738fae08a3a7d4
livesimple02 <shlaupan@yahoo.com>
2021-04-15 17:56:33 -0600

    Complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3{
+  public static void main(String[] args){
+    double num1, num2, num3, average;
+    num1 = 7;
+    num2 = 12.3;
+    num3 = 8;
+    average = (num1 + num2 + num3) / 3;
+    System.out.println(average);
+  }
+}
